317c43b30dfee62854be060c84bf2c73e71cae30
Manodevi Sathishkumar <smanodevi@gmail.com>
2021-11-13 16:20:46 +0400

    Connect mongoose to our database and bring that to in server.js

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const students = require('./routes/students');
 const reportCard = require('./routes/report-card');
 
+const connectDB = require('./config/db');
 const express = require('express');
 
-
 const app = express();
+
+// Connect Database
+connectDB();
+
 app.get('/', (req, res) => res.json({msg: "Welcome to the Student Report Card API..."}));
 
 const PORT = process.env.PORT || 5000;
@@ -14,4 +18,3 @@ app.listen(PORT, () => console.log(`SERVER started on PORT ${PORT}`));
 // Define Routes
 app.use('api/students', students);
 app.use('api/report-card', reportCard);
-
