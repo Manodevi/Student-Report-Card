ed27c5f0113e78ead93c820261fbd9498e1b3d80
Manodevi Sathishkumar <smanodevi@gmail.com>
2021-11-13 16:38:05 +0400

    Added check and validation Result - to validate student param

--- routes/students.js
+++ routes/students.js
@@ -1,11 +1,26 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator');
+
+const Student = require('../model/Student');
 
 // @route   POST api/students 
 // @desc    Add a student
 // @access  Public
-router.post('/', (req, res) => {  
-  res.send('Add a Student');
-});
+router.post(
+    '/', 
+    [
+      check('name', 'Name is required').not().isEmpty(),
+      check('class', 'Class is required').not().isEmpty(),
+      check('section', 'Section is required').not().isEmpty(),
+    ],
+    (req, res) => {  
+      const errors = validationResult(req);
+      if(!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array()});
+      }
+      res.send('Added');
+    }
+  );
 
 module.exports = router;
