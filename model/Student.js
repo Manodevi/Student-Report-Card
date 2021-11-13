dbbbeb8599942198e238566f5df555a22e7e29d0
Manodevi Sathishkumar <smanodevi@gmail.com>
2021-11-13 16:35:55 +0400

    Created Student Schema model

---
+++ model/Student.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const StudentSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  class: {
+    type: Number,
+    required: true
+  },
+  section: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model('student', StudentSchema);
