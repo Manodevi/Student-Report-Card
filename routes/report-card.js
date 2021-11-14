58eb47e897cf7f138794b6b190273ecd4adb1380
Manodevi Sathishkumar <smanodevi@gmail.com>
2021-11-14 09:23:47 +0400

    Update Report Card API

--- routes/report-card.js
+++ routes/report-card.js
@@ -62,5 +62,38 @@ router.get(
     }
   );
 
+// @route   PUT api/report-card/:id
+// @desc    Update Student's Report Card
+// @access  Public
+router.put(
+  '/:id',
+  async (req, res) => {
+    try {
+      console.log('Report Card Update');
+      const { arabic, english, mathematics, uae, french, science } = req.body;
+
+      // Build Report Card object
+      const reportCardFields = {};
+      if(arabic) reportCardFields.arabic = arabic;
+      if(english) reportCardFields.english = english;
+      if(mathematics) reportCardFields.mathematics = mathematics;
+      if(uae) reportCardFields.uae = uae;
+      if(science) reportCardFields.science = science;
+      if(french) reportCardFields.french = french;
+
+      let reportCard = await ReportCard.findById(req.params.id);
+
+      if(!reportCard) return res.status(404).json({ msg: `Report Card not found` });
+
+      reportCard = await ReportCard.findByIdAndUpdate(req.params.id, {
+        $set: reportCardFields });
+
+      res.json(reportCard);
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send('Server Error');
+    }
+  }
+);
 
 module.exports = router;
