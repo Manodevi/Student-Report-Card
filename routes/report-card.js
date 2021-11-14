4132a890df849d6b9a930cac032e234d5aec7f22
Manodevi Sathishkumar <smanodevi@gmail.com>
2021-11-14 04:29:36 +0400

    Added request value validation and saved it in DB
    1. Verified each field to be entered
    2. save the report card with student id

--- routes/report-card.js
+++ routes/report-card.js
@@ -1,11 +1,48 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator');
 
-// @route   POST api/report-card 
-// @desc    Add a report card
+const ReportCard = require('../model/ReportCard');
+
+// @route   POST api/report-card
+// @desc    Add a student
 // @access  Public
-router.post('/', (req, res) => {  
-  res.send('Add a Report Card');
-});
+router.post(
+    '/', 
+    [
+      check('arabic', 'Arabic is required').not().isEmpty(),
+      check('uae', 'UAE is required').not().isEmpty(),
+      check('english', 'English is required').not().isEmpty(),
+      check('mathematics', 'Mathematics is required').not().isEmpty(),
+      check('french', 'French is required').not().isEmpty(),
+      check('science', 'Science is required').not().isEmpty(),
+      check('term', 'TERM is required').not().isEmpty(),
+      check('periodic_test', 'Periodic Test is required').not().isEmpty()      
+    ],
+    async (req, res) => {        
+      try {
+        console.log('Report Card entered');
+        const errors = validationResult(req);
+        if(!errors.isEmpty()) {
+          return res.status(400).json({ errors: errors.array()});
+        }      
+        
+        const { arabic, uae, english,
+          mathematics, french, science, term, periodic_test } = req.body;
+        const student = req.body.id;
+
+        const newReportCard = new ReportCard({ arabic, uae, english,
+          mathematics, french, science, term, periodic_test, student
+        });
+      
+        const reportCard = await newReportCard.save();
+        res.json(reportCard);
+        // res.send('report Added');
+      } catch (error) {
+        console.log(error);
+        res.status(500).send('Server Error');
+      }      
+    }
+  );
 
 module.exports = router;
