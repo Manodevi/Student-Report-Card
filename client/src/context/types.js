7643465ded1c278cfbda217303470bb2a2126cff
Manodevi Sathishkumar <smanodevi@gmail.com>
2021-11-17 18:47:12 +0400

    Student Error - messages

--- client/src/context/types.js
+++ client/src/context/types.js
@@ -7,3 +7,4 @@ export const SET_ALERT = 'SET_ALERT';
 export const REMOVE_ALERT = 'REMOVE_ALERT';
 export const SET_CURRENT = 'SET_CURRENT';
 export const CLEAR_CURRENT = 'CLEAR_CURRENT';
+export const STUDENT_ERROR = 'STUDENT_ERROR';
