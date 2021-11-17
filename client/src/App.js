da85b066d93bf086231531bd9ac3587b1a229ccf
Manodevi Sathishkumar <smanodevi@gmail.com>
2021-11-17 10:44:49 +0400

    Wrapping the student context API from App's child components

--- client/src/App.js
+++ client/src/App.js
@@ -3,19 +3,22 @@ import Navbar from './components/layout/Navbar';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './components/pages/Home';
 import About from './components/pages/About';
+import StudentState from './context/studentReportCard/StudentState';
 
 const App = () => {
   return (
-    <Router>   
-      <Navbar />
-      <div className="container">
-        <Routes>
-          <Route exact path='/' element={<Home/>} />
-          <Route exact path='/about' element={<About/>} />
-          <Route exact path='/' element={<Home/>} />
-        </Routes>      
-      </div>
-    </Router>
+    <StudentState>
+      <Router>   
+        <Navbar />
+        <div className="container">
+          <Routes>
+            <Route exact path='/' element={<Home/>} />
+            <Route exact path='/about' element={<About/>} />
+            <Route exact path='/' element={<Home/>} />
+          </Routes>      
+        </div>
+      </Router>
+    </StudentState>
   );
 }
 
