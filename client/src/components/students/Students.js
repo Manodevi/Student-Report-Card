7d45485c8e316efd2f9d148cc26f82c379e989cd
Manodevi Sathishkumar <smanodevi@gmail.com>
2021-11-17 19:15:43 +0400

    Populate Students list after component mounted

--- client/src/components/students/Students.js
+++ client/src/components/students/Students.js
@@ -1,18 +1,28 @@
-import React, { Fragment, useContext } from 'react';
+import React, { Fragment, useContext, useEffect } from 'react';
 import StudentContext from '../../context/studentReportCard/studentContext';
 import StudentItem from './StudentItem';
 
 const Students = () => {
   const context = useContext(StudentContext);
+  const { students, filtered, getStudents } = context;
 
-  const { students, filtered } = context;
+  useEffect(() => {
+    getStudents();
+    // eslint-disable-next-line
+  }, []);
+
+  /*
+  if (!students) {
+    return <h4>Please Add A Student </h4>;
+  }
+  */ 
   return (
     <Fragment>
       {filtered !== null
         ? filtered.map(student => (
             <StudentItem key={student._id} student={student} />  
         ))
-        : students.map(student => (
+        : students !== null && students.map(student => (
           <StudentItem key={student._id} student={student} />
         ))
       }     
