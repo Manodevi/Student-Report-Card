2fdec88a1c2cfdcf88c90716aacdbb156b650efc
Manodevi Sathishkumar <smanodevi@gmail.com>
2021-11-17 10:40:32 +0400

    Created Context API for Student functionality

---
+++ client/src/context/studentReportCard/StudentState.js
@@ -0,0 +1,79 @@
+import React, { useReducer } from 'react';
+// import uuid from 'uuid';
+import studentContext from "./studentContext";
+import studentReducer from './studentReducer';
+import {
+    ADD_STUDENT,
+    DELETE_STUDENT,
+    UPDATE_STUDENT,
+    FILTER_STUDENTS,
+    CLEAR_FILTER,
+    SET_ALERT,
+    REMOVE_ALERT
+} from '../types' ;
+
+const StudentState = props => {
+  const initialState = {
+    students: [
+      {
+        id: 2,
+        name: 'Vaishu',
+        std: '1',
+        section: 'B'
+      },
+      {
+        id: 4,
+        name: 'DDDDD',
+        std: '5',
+        section: 'D'
+      },
+      {
+        id: 8,
+        name: 'XXXXZZZZ',
+        std: '10',
+        section: 'A'
+      }
+    ]
+  };
+
+  const [ state, dispatch ] = useReducer(studentReducer, initialState);
+
+  // Add Student
+  const addStudent = () => {
+    dispatch(ADD_STUDENT);
+  };
+/*
+  // Delete Student
+  const deleteStudent = () => {
+    dispatch(DELETE_STUDENT);
+  };
+
+  // Set Current Student
+  const currentStudent = () => {
+    dispatch(ADD_STUDENT);
+  };
+  // Add 
+  const addStudent = () => {
+    dispatch(ADD_STUDENT);
+  };
+  // Add Student
+  const addStudent = () => {
+    dispatch(ADD_STUDENT);
+  };
+  // Add Student
+  const addStudent = () => {
+    dispatch(ADD_STUDENT);
+  };
+  */
+
+  return (
+    <studentContext.Provider value={{
+      students: state.students,
+      addStudent
+    }}>
+      {props.children}
+    </studentContext.Provider>
+  );
+};
+
+export default StudentState;
---
+++ client/src/context/types.js
@@ -0,0 +1,7 @@
+export const ADD_STUDENT = 'ADD_STUDENT';
+export const DELETE_STUDENT = 'DELETE_STUDENT';
+export const UPDATE_STUDENT = 'UPDATE_STUDENT';
+export const FILTER_STUDENTS = 'FILTER_STUDENTS';
+export const CLEAR_FILTER = 'CLEAR_FILTER';
+export const SET_ALERT = 'SET_ALERT';
+export const REMOVE_ALERT = 'REMOVE_ALERT';
