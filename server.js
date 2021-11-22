f98c810964b7b0d0b1bcbcbe3f33bf019da06dc2
Manodevi Sathishkumar <smanodevi@gmail.com>
2021-11-22 19:48:32 +0400

    Preparing for deployment

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const reportCard = require('./routes/report-card');
 
 const connectDB = require('./config/db');
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
@@ -11,11 +12,22 @@ connectDB();
 
 app.use(express.json({ extended: false })); // now can accept body data
 
-app.get('/', (req, res) => res.json({msg: "Welcome to the Student Report Card API..."}));
+app.get('/', (req, res) => {
+    // res.json({msg: "Welcome to the Student Report Card API..."})
+  }
+);
 
 // Define Routes
 app.use('/api/students', students);
 app.use('/api/report-card', reportCard);
+
+// Serve Static assets in production
+if (process.env.NODE.ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
+}
 
 const PORT = process.env.PORT || 5000;
 
