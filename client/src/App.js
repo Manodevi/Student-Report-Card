ddfba676cad8a15c0d74efa98cf621397341b477
Manodevi Sathishkumar <smanodevi@gmail.com>
2021-11-22 18:49:45 +0400

    Added Edit Student Route and Alert Component

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,9 @@ import Navbar from './components/layout/Navbar';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './components/pages/Home';
 import About from './components/pages/About';
+import Alert from './components/layout/Alert';
+import ReportCard from './components/reportCard/ReportCard';
+import AddEditStudent from './components/students/AddEditStudent';
 import StudentState from './context/studentReportCard/StudentState';
 
 const App = () => {
@@ -11,9 +14,12 @@ const App = () => {
       <Router>   
         <Navbar />
         <div className="container">
+          <Alert />
           <Routes>
-            <Route exact path='/' element={<Home/>} />
-            <Route exact path='/about' element={<About/>} />
+            <Route exact path='/' element={<Home />} />
+            <Route exact path='/about' element={<About />} />
+            <Route exact path='/students/:id' element={<AddEditStudent />} />
+            <Route exact path='/students/:id/report-card' element={<ReportCard />} />              
             <Route exact path='/' element={<Home/>} />
           </Routes>      
         </div>
